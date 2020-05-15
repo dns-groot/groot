bf835c1e61b5dbd938dd0d598a5cf1a34b91fbf0
sivakesava <sivakesava1@gmail.com>
2020-05-14 17:08:06 -0700

    Test folder fixed Updated to real domain "cc.il.us."

--- test/demo-test.cpp
+++ test/demo-test.cpp
@@ -8,25 +8,25 @@ BOOST_AUTO_TEST_CASE(demo_test)
     Driver driver;
     DriverTest dt;
     boost::filesystem::path directory("TestFiles");
-    std::ifstream metadataFile((directory / "net.sy" / "zone_files" / "metadata.json").string());
+    std::ifstream metadataFile((directory / "cc.il.us" / "zone_files" / "metadata.json").string());
     json metadata;
     metadataFile >> metadata;
 
-    std::ifstream i((directory / "net.sy" / "jobs.json").string());
+    std::ifstream i((directory / "cc.il.us" / "jobs.json").string());
     json j;
     i >> j;
 
-    long total_rrs_parsed = driver.SetContext(metadata, (directory / "net.sy" / "zone_files").string());
+    long total_rrs_parsed = driver.SetContext(metadata, (directory / "cc.il.us" / "zone_files").string());
 
-    BOOST_TEST(102 == total_rrs_parsed);
-    BOOST_TEST(43 == dt.GetNumberofLabelGraphVertices(driver));
-    BOOST_TEST(43 == dt.GetNumberofLabelGraphEdges(driver));
+    BOOST_TEST(140 == total_rrs_parsed);
+    BOOST_TEST(70 == dt.GetNumberofLabelGraphVertices(driver));
+    BOOST_TEST(70 == dt.GetNumberofLabelGraphEdges(driver));
 
     boost::unordered_map<string, long> types_to_count = dt.GetTypeToCountMap(driver);
 
     BOOST_TEST(1 == types_to_count["Wildcard"]);
     BOOST_TEST(1 == types_to_count["DNAME"]);
-    BOOST_TEST(3 == types_to_count["CNAME"]);
+    BOOST_TEST(5 == types_to_count["CNAME"]);
 
     long total_ecs = 0;
 
@@ -35,11 +35,11 @@ BOOST_AUTO_TEST_CASE(demo_test)
         driver.GenerateECsAndCheckProperties();
         total_ecs += driver.GetECCountForCurrentJob();
     }
-    BOOST_TEST(120 == total_ecs);
-    BOOST_TEST(48 == dt.GetNumberofViolations(driver));
+    BOOST_TEST(176 == total_ecs);
+    BOOST_TEST(20 == dt.GetNumberofViolations(driver));
 
     EC test_ec;
-    test_ec.name = LabelUtils::StringToLabels("bar.trial.net.sy.");
+    test_ec.name = LabelUtils::StringToLabels("ds3.trial.cc.il.us.");
     test_ec.rrTypes.set(RRType::A);
     interpretation::Graph ig = dt.CreateAnInterpretationGraph(driver, test_ec);
     ig.GenerateDotFile("test_ig.dot"); // TODO: Check if file is generated properly
