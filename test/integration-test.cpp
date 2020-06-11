8f57502b829f12f0dee3b67e03833eb2088c8f51
Ryan Beckett <rybecket@microsoft.com>
2020-06-11 13:05:14 -0700

    add integration tests by comparing actual with expected output

---
+++ test/integration-test.cpp
@@ -0,0 +1,61 @@
+#include "driver-test.h"
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include <iterator>
+#include <iostream>
+#include <vector>
+
+namespace fs = boost::filesystem;
+
+BOOST_AUTO_TEST_SUITE(IntegrationTestSuite)
+
+BOOST_AUTO_TEST_CASE(integration_test)
+{
+    Driver driver;
+    DriverTest dt;
+    fs::path directory("TestFiles");
+    auto test_directory = directory / "integration_tests";
+
+    fs::directory_iterator it{test_directory};
+    while (it != fs::directory_iterator{}) {
+        auto entry = *it++;
+        if (fs::is_directory(entry)) {
+            auto test_directory = entry.path();
+            auto zone_path = test_directory / "zone_files";
+            auto metadata_path = zone_path / "metadata.json";
+            auto output_path = test_directory / "output.json";
+            auto expected_path = test_directory / "expected.json";
+            auto jobs_path = test_directory / "jobs.json";
+
+            // read in the metadata file
+            std::ifstream metadataFile(metadata_path.string());
+            json metadata;
+            metadataFile >> metadata;
+
+            // read in the jobs file
+            std::ifstream i(jobs_path.string());
+            json j;
+            i >> j;
+
+            driver.SetContext(metadata, zone_path.string(), false);
+
+            for (auto &user_job : j) {
+                driver.SetJob(user_job);
+                driver.GenerateECsAndCheckProperties();
+            }
+
+            driver.WriteViolationsToFile(output_path.string());
+
+            // compare the actual output with the expected output
+            std::ifstream actual(output_path.string());
+            std::string s1((std::istreambuf_iterator<char>(actual)), std::istreambuf_iterator<char>());
+
+            std::ifstream expected(expected_path.string());
+            std::string s2((std::istreambuf_iterator<char>(expected)), std::istreambuf_iterator<char>());
+
+            BOOST_CHECK_EQUAL(s1, s2);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
